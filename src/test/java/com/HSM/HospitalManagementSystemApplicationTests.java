8c1004f158df020672c55d0f0c35e511c6a72911
vinayn145 <vinayn145@example.com>
2023-07-06 19:51:29 +0530

    hospital management system home page and login module

---
+++ src/test/java/com/HSM/HospitalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HSM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
